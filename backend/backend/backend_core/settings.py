0b734317727ceae5b1533c1ec80002d681c4da68
AMROUCHE Maroua <km_amrouche@esi.dz>
2022-12-29 20:20:32 +0100

    adding created and installed apps to settings.py

--- backend/backend/backend_core/settings.py
+++ backend/backend/backend_core/settings.py
@@ -37,9 +37,17 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #my apps 
+    'api',
+
+    #third party apps
+    'rest-framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -121,3 +129,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+#React’s port through which requests will come be served
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:3000',
+]

e6a0ec5d8ad2020fad5e626a61d04dbc626c8fbd
AMROUCHE Maroua <km_amrouche@esi.dz>
2022-12-31 14:15:08 +0100

    creating urls.py
    adding urls for each model to manipulate eachone in an api that contain the emethods put , post delete

---
+++ backend/api/urls.py
@@ -0,0 +1,19 @@
+from api.views import *
+from rest_framework.routers import DefaultRouter
+from api import views
+from rest_framework import routers
+
+router = routers.SimpleRouter()
+router.register(r'annonces', views.AnnonceViewSet, basename='annonce')
+router.register(r'users', views.UserViewSet, basename='user')
+router.register(r'communes', views.CommuneViewSet, basename='commune')
+router.register(r'wilayas', views.WilayaViewSet, basename='wilaya')
+router.register(r'categories', views.CategorieViewSet, basename='categorie')
+router.register(r'types', views.TypeViewSet, basename='type')
+router.register(r'positions', views.PositionViewSet, basename='position')
+router.register(r'favoris', views.FavoriViewSet, basename='favori')
+router.register(r'messagesOffres', views.MessgeOffreViewSet, basename='messageOffre')
+router.register(r'admins', views.AdminViewSet, basename='admin')
+router.register(r'photos', views.PhotoViewSet, basename='photo')
+
+urlpatterns = router.urls

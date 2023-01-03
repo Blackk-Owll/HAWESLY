0ba5ace15b57ff9feec567c00a6ad1dc04c58684
AMROUCHE Maroua <km_amrouche@esi.dz>
2023-01-03 09:37:27 +0100

    Update API.js

--- frontend/src/API.js
+++ frontend/src/API.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export default axios.create({
+export default  axios.create({
     baseURL: "http://127.0.0.1:8000/api/",
     headers: {
         'Accept':'application/json',
